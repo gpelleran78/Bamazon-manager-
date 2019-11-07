b59bdbb6a9dfd93dbaf4839a2be7933fb90b92fb
gpelleran78 <geremy_pelleran@yahoo.com>
2019-11-07 15:58:23 -0700

    added npm mysql and npm requirer

--- bamazon_manager.js
+++ bamazon_manager.js
@@ -5,8 +5,8 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "ABCefg1978!",
-    database: "bamazon"
+    password: "QAZwsx1234",
+    database: "bamazon_db"
 });
 
 let makeTable = function(){
@@ -15,8 +15,15 @@ let makeTable = function(){
         console.log("ItemID\tProduct Name\tDepartment Name\tPrice\tNumber in Stock");
         console.log("===================================");
         for(var i=0; i<res.length; i++){
-            console.log(res[i].itemid+"\t"+res[i].product.name+"\t"+res[i].departmentname+"\t"+res[i].price+"\t"+res[i].stockquanity);
+            console.log(res[i].item_id+"\t"+res[i].product_name+"\t"+res[i].department_name+"\t"+res[i].price+"\t"+res[i].stock_quanity);
         }
         console.log("====================================");
     })
+    promptManager(res);
 }
+
+var promptManager = function(){
+    inquirer.prompt()
+}
+
+makeTable();
